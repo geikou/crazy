f6385fc293f9b7da1c0c86ca1da96dfb7fdf799b
松本 和馬 <hello@mtmtkzm.com>
2016-10-04 16:02:03 +0900

    headerの大小切り替えを.fixedで対応。（アニメーション未実装）

--- app/source/js/app.js
+++ app/source/js/app.js
@@ -1,53 +1,23 @@
-
-// instagram 流し
+/*
+  Instagram フィードを流す
+*** */
 var instaMargin = 0;
 setInterval(function(){
-	instaMargin--;
-	$('.js-slider').css({
-		'margin-top': instaMargin
-	});
+  instaMargin--;
+  $('.js-slider').css({
+    'margin-top': instaMargin
+  });
 }, 40);
 
 
-// topAnimation
-
-function topAnime() {
-	// 左側を動かす
-	$('.js-left').animate({
-		'left': '35vw'
-	}, 1000);
-
-	// 右側を動かす
-	$('.js-right').animate({
-		'right': '35vw'
-	}, 1000);
-}
-
-setTimeout(function() {
-	topAnime();
-	setTimeout(function() {
-		// 重なったら
-		$('.js-circle').css({
-			'color': 'rgba(0,0,0,0)'
-		});
-
-		$('.js-top-logo').animate({
-			'opacity': '1'
-		}, 500);
-
-		setTimeout(function() {
-			$('.js-left').hide();
-			$('.js-right').animate({
-
-			}, 1000);
-			$('.js-right').animate({
-				'width': '200vw',
-				'height': '200vw',
-				'right': '-50vw',
-				'top': '-50vw'
-			}, 500, function() {
-				$('.topAnime').fadeOut();
-			});			
-		}, 1000);
-	}, 2000);
-}, 2000);
+/*
+  headerのスクロール制御
+*** */
+var $header = $('.header');
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 0) {
+    $header.addClass('fixed');
+  } else {
+    $header.removeClass('fixed');
+  }
+});
